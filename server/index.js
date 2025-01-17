6ff33e0ce09a8fb9cdf9702a01fe230ac03a58ca
Yogesh Tomar <kshanu385@gmail.com>
2025-01-17 08:54:17 +0530

    params query

--- server/index.js
+++ server/index.js
@@ -8,6 +8,20 @@ app.get(`/`, (req, res) => {
   res.send(`<h1>Server is working</h1>`);
 });
 
+app.get(`/params/:name/:place`, (req, res) => {
+  // console.log(req.params);
+  res.send(
+    `<h1>My name is ${req.params.name} and I'm from ${req.params.place}</h1>`
+  );
+});
+
+app.get(`/query`, (req, res) => {
+  // console.log(req.query);
+  res.send(
+    `<h1>My name is ${req.query.name} and I'm from ${req.query.place} and I'm ${req.query.age}</h1>`
+  );
+});
+
 app.listen(PORT, () => {
   console.log(`Server is up and running at http://localhost:${PORT}`);
 });
